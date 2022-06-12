368b034dea6adf4066784c89370c50e25cb5f64a
todelusion <todelusion@gmail.com>
2022-06-12 12:14:09 +0800

    homePage_finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="landingWrap">
+    <div class="landingWrap xl:mb-44">
       <div class="titleWrap mx-auto">
         <h1 class="text-primary text-base xl:text-[1.25rem] mb-2">
           ChatTalker 最全面的聊天機器人
@@ -32,222 +32,242 @@
         </div>
       </div>
     </div>
-    <div class="subTitleWrap">
-      <h2 class="font-bold text-[1.75rem] text-center mb-8">
-        三大平台，我來搞定
-      </h2>
-    </div>
-    <div class="platformWrap mb-[3.75rem]">
-      <ul class="platformCard mb-6">
-        <li class="mb-4">
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/messenger.svg"
-            alt="messenger"
-            width="90"
-            class="w-[60px]"
-          />
-        </li>
-        <li class="font-bold text-lg mb-4">Facebook Messenger</li>
-        <li class="text-lightDark">
-          結合粉絲專頁，提高粉絲互動率<br />有效經營品牌社群
-        </li>
-      </ul>
-      <ul class="platformCard mb-6">
-        <li class="mb-4">
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/line.svg"
-            alt="line"
-            width="90"
-            class="w-[60px]"
-          />
-        </li>
-        <li class="font-bold text-lg mb-4">LINE 官方帳號</li>
-        <li class="text-lightDark">
-          融入日常生活，精準傳遞品牌資訊<br />打造個人化專屬服務
-        </li>
-      </ul>
-      <ul class="platformCard">
-        <li class="mb-4">
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/instagram.svg"
-            alt="instagram.svg"
-            width="90"
-            class="w-[60px]"
-          />
-        </li>
-        <li class="font-bold text-lg mb-4">Instagram 商業帳號</li>
-        <li class="text-lightDark">
-          貼近粉絲，提升品牌自然聲量<br />行銷、抽獎一站完成
-        </li>
-      </ul>
-    </div>
-    <div class="subTitleWrap">
-      <h2 class="font-bold text-[1.75rem] mb-4">任何的角色，我都能給</h2>
-      <p class="text-lg mb-8">
-        量身打造您想要的專屬服務，幫助您迅速判斷成效、並加快決策
-      </p>
-    </div>
-    <div class="taWrap mb-14">
-      <ul class="taCard mb-6">
-        <li>
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/ch01.svg"
-            class="rounded-t-xl"
-          />
-        </li>
-        <li class="taCardInfo">
-          <h3 class="font-bold text-primary mb-4">活動主</h3>
-          <p>用戶數位足跡掌握在手<br />精準取得客戶輪廓，提供分析</p>
-        </li>
-      </ul>
-      <ul class="taCard mb-6">
-        <li>
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/ch02.svg"
-            class="rounded-t-xl"
-          />
-        </li>
-        <li class="taCardInfo">
-          <h3 class="font-bold text-primary mb-4">創作者</h3>
-          <p>自由設計互動腳本<br />輕鬆整合各項數據</p>
-        </li>
-      </ul>
-      <ul class="taCard mb-6">
-        <li>
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/ch03.svg"
-            class="rounded-t-xl"
-          />
-        </li>
-        <li class="taCardInfo">
-          <h3 class="font-bold text-primary mb-4">企業品牌</h3>
-          <p>多元資料整合，做出服務差異化 幫助企業加速決策</p>
-        </li>
-      </ul>
-      <ul class="taCard">
-        <li>
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/ch04.svg"
-            class="rounded-t-xl"
-          />
-        </li>
-        <li class="taCardInfo">
-          <h3 class="font-bold text-primary mb-4">其他</h3>
-          <p>創造更多可能...</p>
-        </li>
-      </ul>
-    </div>
-    <div class="subTitleWrap">
-      <h2 class="font-bold text-[1.75rem] mb-4">
-        快速了解客戶輪廓<br />建立品牌流量池
-      </h2>
-      <p class="mb-4 max-w-[335px] mx-auto">
-        深入用戶日常，成為用戶貼身好友 精準傳遞品牌資訊，打造個人化專屬服務
-        不知不覺深入用戶生活，成為用戶貼心的好朋朋:D
-      </p>
-    </div>
-    <div class="planButton bg-primary mb-8 mx-auto">
-      <router-link to="/price">方案費用</router-link>
-    </div>
-    <div class="featureWrap mb-[3.75rem]">
-      <ul class="featureCard mb-6">
-        <li class="iconWrap mb-6">
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon01.svg"
-            width="52"
-          />
-        </li>
-        <li class="featureCardInfo">
-          <h3 class="font-bold text-dark text-lg mb-4">無敵轉換術</h3>
-          <p>精準掌握客群來源<br />分析用戶轉化流程</p>
-        </li>
-      </ul>
-      <ul class="featureCard mb-6">
-        <li class="iconWrap mb-6">
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon03.svg"
-            width="52"
-          />
-        </li>
-        <li class="featureCardInfo">
-          <h3 class="font-bold text-dark text-lg mb-4">一站式訊息中心</h3>
-          <p>整合Messenger、LINE<br />和IG三大平台訊息</p>
-        </li>
-      </ul>
-      <ul class="featureCard mb-6">
-        <li class="iconWrap mb-6">
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon02.svg"
-            width="52"
-          />
-        </li>
-        <li class="featureCardInfo">
-          <h3 class="font-bold text-dark text-lg mb-4">聊天機器人</h3>
-          <p>66款聊天機器人模組<br />自由設計聊天流程</p>
-        </li>
-      </ul>
-      <ul class="featureCard mb-6">
-        <li class="iconWrap mb-6">
-          <img
-            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon04.svg"
-            width="52"
-          />
-        </li>
-        <li class="featureCardInfo">
-          <h3 class="font-bold text-dark text-lg mb-4">群發訊息</h3>
-          <p>自由選定特定受眾<br />精準推送群發訊息</p>
-        </li>
-      </ul>
-    </div>
-    <div class="subTitleWrap">
-      <h2 class="font-bold text-[1.75rem] mb-20">好評如潮</h2>
-    </div>
-    <div class="commentWrap mb-[3.75rem]">
-      <ul class="commentCard relative">
-        <li class="absolute top-[-40px] left-8">
-          <img
-            src="https://github.com/hexschool/webLayoutTraining1st/blob/master/chatTalker_images/user01.jpg?raw=true"
-            alt=""
-            class="rounded-full w-20"
-          />
-        </li>
-        <li>
-          原來聊天機器人可以這麼有人性！團隊非常的專業，有完整的行銷計畫提供數位整合，讓我們公司的產品用更活潑的方式讓使用者認識。
-        </li>
-        <li class="border-t-2 border-gray-300 my-4"></li>
-        <li class="font-bold m-2">清心寡慾設計公司</li>
-        <li class="commentInfo m-2">
-          <p>Lina 執行長</p>
-          <div class="commentStar">
+    <section class="max-w-[1088px]">
+      <div class="subTitleWrap">
+        <h2 class="font-bold text-[1.75rem] xl:text-3xl text-center mb-8">
+          三大平台，我來搞定
+        </h2>
+      </div>
+      <div class="platformWrap mb-[3.75rem] xl:mb-24">
+        <ul class="platformCard mb-6">
+          <li class="mb-4">
             <img
-              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
-              alt=""
-              class="ml-2"
+              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/messenger.svg"
+              alt="messenger"
+              width="90"
+              class="w-[60px] xl:w-[90px]"
             />
+          </li>
+          <li class="font-bold text-lg mb-4">Facebook Messenger</li>
+          <li class="text-lightDark">
+            結合粉絲專頁，提高粉絲互動率<br />有效經營品牌社群
+          </li>
+        </ul>
+        <ul class="platformCard mb-6 xl:ml-32">
+          <li class="mb-4">
             <img
-              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
-              alt=""
-              class="ml-2"
+              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/line.svg"
+              alt="line"
+              width="90"
+              class="w-[60px] xl:w-[90px]"
             />
+          </li>
+          <li class="font-bold text-lg mb-4">LINE 官方帳號</li>
+          <li class="text-lightDark">
+            融入日常生活，精準傳遞品牌資訊<br />打造個人化專屬服務
+          </li>
+        </ul>
+        <ul class="platformCard xl:ml-32">
+          <li class="mb-4">
             <img
-              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
-              alt=""
-              class="ml-2"
+              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/instagram.svg"
+              alt="instagram.svg"
+              width="90"
+              class="w-[60px] xl:w-[90px]"
             />
+          </li>
+          <li class="font-bold text-lg mb-4">Instagram 商業帳號</li>
+          <li class="text-lightDark">
+            貼近粉絲，提升品牌自然聲量<br />行銷、抽獎一站完成
+          </li>
+        </ul>
+      </div>
+      <div class="subTitleWrap">
+        <h2 class="font-bold text-[1.75rem] mb-4 xl:text-3xl">
+          任何的角色，我都能給
+        </h2>
+        <p class="text-lg mb-8">
+          量身打造您想要的專屬服務，幫助您迅速判斷成效、並加快決策
+        </p>
+      </div>
+      <div class="taWrap mb-14 xl:mb-24">
+        <ul class="taCard mb-6 xl:ml-4">
+          <li>
             <img
-              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
-              alt=""
-              class="ml-2"
+              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/ch01.svg"
+              class="rounded-t-xl"
+              width="260"
             />
+          </li>
+          <li class="taCardInfo">
+            <h3 class="font-bold text-primary mb-4">活動主</h3>
+            <p>用戶數位足跡掌握在手<br />精準取得客戶輪廓，提供分析</p>
+          </li>
+        </ul>
+        <ul class="taCard mb-6 xl:ml-4">
+          <li>
             <img
-              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
-              alt=""
-              class="ml-2"
+              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/ch02.svg"
+              width="260"
+              class="rounded-t-xl"
             />
+          </li>
+          <li class="taCardInfo">
+            <h3 class="font-bold text-primary mb-4">創作者</h3>
+            <p>自由設計互動腳本<br />輕鬆整合各項數據</p>
+          </li>
+        </ul>
+        <ul class="taCard mb-6 xl:ml-4">
+          <li>
+            <img
+              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/ch03.svg"
+              class="rounded-t-xl"
+              width="260"
+            />
+          </li>
+          <li class="taCardInfo">
+            <h3 class="font-bold text-primary mb-4">企業品牌</h3>
+            <p>多元資料整合，做出服務差異化 幫助企業加速決策</p>
+          </li>
+        </ul>
+        <ul class="taCard mb-6 xl:ml-4">
+          <li>
+            <img
+              src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/ch04.svg"
+              class="rounded-t-xl"
+              width="260"
+            />
+          </li>
+          <li class="taCardInfo">
+            <h3 class="font-bold text-primary mb-4">其他</h3>
+            <p>創造更多可能...</p>
+          </li>
+        </ul>
+      </div>
+      <div class="featureWrap mb-[3.75rem] xl:mb-24">
+        <div class="xl:w-max">
+          <div class="subTitleWrap xl:text-start">
+            <h2 class="font-bold text-[1.75rem] mb-4 text-3xl">
+              快速了解客戶輪廓<br />建立品牌流量池
+            </h2>
+            <p class="mb-4 max-w-[335px] mx-auto xl:mx-0">
+              深入用戶日常，成為用戶貼身好友
+              精準傳遞品牌資訊，打造個人化專屬服務
+              不知不覺深入用戶生活，成為用戶貼心的好朋朋:D
+            </p>
           </div>
-        </li>
-      </ul>
-    </div>
+          <div class="planButton w-32 bg-primary mb-8 mx-auto xl:mx-0">
+            <router-link to="/price">方案費用</router-link>
+          </div>
+        </div>
+        <div class="xl:w-1/2 xl:flex xl:flex-wrap xl:items-center">
+          <div class="xl:w-1/2 xl:h-min">
+            <ul class="featureCard mb-6">
+              <li class="iconWrap mb-6">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon01.svg"
+                  width="52"
+                />
+              </li>
+              <li class="featureCardInfo">
+                <h3 class="font-bold text-dark text-lg mb-4">無敵轉換術</h3>
+                <p>精準掌握客群來源<br />分析用戶轉化流程</p>
+              </li>
+            </ul>
+          </div>
+          <div class="xl:w-1/2 xl:h-min">
+            <ul class="featureCard mb-6">
+              <li class="iconWrap mb-6">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon03.svg"
+                  width="52"
+                />
+              </li>
+              <li class="featureCardInfo">
+                <h3 class="font-bold text-dark text-lg mb-4">一站式訊息中心</h3>
+                <p>整合Messenger、LINE<br />和IG三大平台訊息</p>
+              </li>
+            </ul>
+          </div>
+          <div class="xl:w-1/2">
+            <ul class="featureCard mb-6">
+              <li class="iconWrap mb-6">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon02.svg"
+                  width="52"
+                />
+              </li>
+              <li class="featureCardInfo">
+                <h3 class="font-bold text-dark text-lg mb-4">聊天機器人</h3>
+                <p>66款聊天機器人模組<br />自由設計聊天流程</p>
+              </li>
+            </ul>
+          </div>
+          <ul class="featureCard mb-6">
+            <li class="iconWrap mb-6">
+              <img
+                src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon04.svg"
+                width="52"
+              />
+            </li>
+            <li class="featureCardInfo">
+              <h3 class="font-bold text-dark text-lg mb-4">群發訊息</h3>
+              <p>自由選定特定受眾<br />精準推送群發訊息</p>
+            </li>
+          </ul>
+          <div class="xl:w-1/2"></div>
+        </div>
+      </div>
+      <div class="subTitleWrap">
+        <h2 class="font-bold text-[1.75rem] mb-20">好評如潮</h2>
+      </div>
+      <div class="commentWrap mb-[3.75rem]">
+        <ul class="commentCard relative">
+          <li class="absolute top-[-40px] left-8">
+            <img
+              src="https://github.com/hexschool/webLayoutTraining1st/blob/master/chatTalker_images/user01.jpg?raw=true"
+              alt=""
+              class="rounded-full w-20"
+            />
+          </li>
+          <li>
+            原來聊天機器人可以這麼有人性！團隊非常的專業，有完整的行銷計畫提供數位整合，讓我們公司的產品用更活潑的方式讓使用者認識。
+          </li>
+          <li class="border-t-2 border-gray-300 my-4"></li>
+          <li class="font-bold m-2">清心寡慾設計公司</li>
+          <li class="commentInfo m-2">
+            <p>Lina 執行長</p>
+            <div class="commentStar">
+              <img
+                src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
+                alt=""
+                class="ml-2"
+              />
+              <img
+                src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
+                alt=""
+                class="ml-2"
+              />
+              <img
+                src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
+                alt=""
+                class="ml-2"
+              />
+              <img
+                src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
+                alt=""
+                class="ml-2"
+              />
+              <img
+                src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_star.svg"
+                alt=""
+                class="ml-2"
+              />
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
   </div>
   <footer>
     <img
@@ -258,9 +278,9 @@
     <img
       src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/home_footer.svg"
       alt="footer"
-      class="w-full mb-10 hidden sm:block"
+      class="w-full mb-10 xl:mb-[-7rem] hidden sm:block"
     />
-    <h2 class="font-bold mb-6 text-2xl text-center">
+    <h2 class="font-bold mb-6 text-2xl text-center xl:text-3xl">
       \ 美好用戶體驗，從對話開始 /
     </h2>
     <div class="planButton w-[136px] bg-primary mb-8 mx-auto">
