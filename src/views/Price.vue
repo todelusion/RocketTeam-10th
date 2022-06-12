4beb684263e6faf4e6c1a0acc9dddc237f8135b2
todelusion <todelusion@gmail.com>
2022-06-13 00:08:30 +0800

    chartTalker_ver1

--- src/views/Price.vue
+++ src/views/Price.vue
@@ -11,7 +11,7 @@
   <div
     class="bg-[#F7F9FD] h-[495px] w-full absolute z-[-2] top-0 rounded-bl-[10rem] xl:h-screen"
   ></div>
-  <div class="container max-w-full mx-0 pt-[7rem] sm:pt-0">
+  <div class="container max-w-full mx-0 pt-[7rem] pb-24">
     <div class="landingWrap px-0 relative xl:mb-4 h-screen mb-10">
       <div class="titleWrap px-5 xl:px-36">
         <p
@@ -34,93 +34,211 @@
       <div class="userBtn">25000位</div>
       <div class="userBtn">>25000位</div>
     </div>
-    <div class="planWrap">
-      <div class="max-w-4xl mx-auto xl:flex">
-        <div class="planCard mb-6">
+    <div class="planWrap mb-14">
+      <div class="max-w-4xl mx-auto xl:flex xl:items-center xl:h-full">
+        <div class="planCard mb-6 lg:mb-0">
           <div class="border-b-gray-200 border-b-2 p-4">
             <h3 class="text-2xl font-bold text-center">基本版</h3>
           </div>
           <div class="px-4 py-6">
-            <p class="mb-6">
-              提供中小企業最必要的功能 完整建立分眾以達到精準的行銷自動化
+            <p class="mb-6 text-center">
+              提供中小企業最必要的功能<br>完整建立分眾以達到精準的行銷自動化
             </p>
             <ul class="flex items-center justify-around w-full mb-7">
-              <li class="text-xs text-center w-5/12 py-4 bg-[#F5F5F5] rounded-xl">
+              <li
+                class="text-xs text-center w-5/12 py-4 bg-[#F5F5F5] rounded-xl"
+              >
                 訂閱用戶數<br /><span class="text-3xl text-primary">10000</span
                 ><span class="text-primary">位</span>
               </li>
               <li>=</li>
-              <li class="text-xs text-center w-5/12 py-4 bg-[#F5F5F5] rounded-xl">
-                試算每月費用<br /><span class="text-3xl">600</span
-                >位
+              <li
+                class="text-xs text-center w-5/12 py-4 bg-[#F5F5F5] rounded-xl"
+              >
+                試算每月費用<br /><span class="text-3xl">600</span>位
               </li>
             </ul>
             <ul class="mb-9">
               <li class="flex mb-4">
-                <img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg" alt="">
-                <p class="text-sm ml-4">支援 Messenger / Instagram 或 LINE 渠道</p>
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg"
+                  alt=""
+                />
+                <p class="text-sm ml-4">
+                  支援 Messenger / Instagram 或 LINE 渠道
+                </p>
               </li>
               <li class="flex mb-4">
-                <img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg" alt="">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg"
+                  alt=""
+                />
                 <p class="text-sm ml-4">10 個進階套件</p>
               </li>
               <li class="flex mb-4">
-                <img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg" alt="">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg"
+                  alt=""
+                />
                 <p class="text-sm ml-4">可設定 50 種分眾標籤</p>
               </li>
               <li class="flex mb-4">
-                <img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg" alt="">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg"
+                  alt=""
+                />
                 <p class="text-sm ml-4">客服人機轉接</p>
               </li>
             </ul>
             <div class="w-full flex justify-center">
-              <button class="text-white text-lg bg-primary px-8 py-3 rounded-3xl font-light cursor-pointer duration-100">立即體驗</button>
+              <button
+                class="text-white text-lg bg-primary px-8 py-3 rounded-3xl font-light cursor-pointer duration-100"
+              >
+                立即體驗
+              </button>
             </div>
           </div>
         </div>
         <div class="planCard">
           <div class="border-b-gray-200 border-b-2 p-4">
-            <h3 class="text-2xl font-bold text-center">基本版</h3>
+            <h3 class="text-2xl font-bold text-center">標準版</h3>
           </div>
           <div class="px-4 py-6">
-            <p class="mb-6">
-              提供中小企業最必要的功能 完整建立分眾以達到精準的行銷自動化
+            <p class="mb-6 text-center">
+              建立企業流量池<br>全通路數據整合與交換 高可靠性服務
             </p>
             <ul class="flex items-center justify-around w-full mb-7">
-              <li class="text-xs text-center w-5/12 py-4 bg-[#F5F5F5] rounded-xl">
+              <li
+                class="text-xs text-center w-5/12 py-4 bg-[#F5F5F5] rounded-xl"
+              >
                 訂閱用戶數<br /><span class="text-3xl text-primary">10000</span
                 ><span class="text-primary">位</span>
               </li>
               <li>=</li>
-              <li class="text-xs text-center w-5/12 py-4 bg-[#F5F5F5] rounded-xl">
-                試算每月費用<br /><span class="text-3xl">600</span
-                >位
+              <li
+                class="text-xs text-center w-5/12 py-4 bg-[#F5F5F5] rounded-xl"
+              >
+                試算每月費用<br /><span class="text-3xl">1600</span>位
               </li>
             </ul>
             <ul class="mb-9">
               <li class="flex mb-4">
-                <img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg" alt="">
-                <p class="text-sm ml-4">支援 Messenger / Instagram 或 LINE 渠道</p>
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg"
+                  alt=""
+                />
+                <p class="text-sm ml-4">
+                  支援多個 Messenger、Instagram 和 LINE 渠道
+                </p>
               </li>
               <li class="flex mb-4">
-                <img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg" alt="">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg"
+                  alt=""
+                />
                 <p class="text-sm ml-4">10 個進階套件</p>
               </li>
               <li class="flex mb-4">
-                <img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg" alt="">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg"
+                  alt=""
+                />
                 <p class="text-sm ml-4">可設定 50 種分眾標籤</p>
               </li>
               <li class="flex mb-4">
-                <img src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg" alt="">
+                <img
+                  src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_check.svg"
+                  alt=""
+                />
                 <p class="text-sm ml-4">客服人機轉接</p>
               </li>
             </ul>
             <div class="w-full flex justify-center">
-              <button class="text-white text-lg bg-primary px-8 py-3 rounded-3xl font-light cursor-pointer duration-100">立即體驗</button>
+              <button
+                class="text-white text-lg bg-primary px-8 py-3 rounded-3xl font-light cursor-pointer duration-100"
+              >
+                立即體驗
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <h2 class="font-bold text-[1.75rem] xl:text-3xl text-center mb-8 w-full">
+      常見問題
+    </h2>
+    <div class="px-5">
+      <ul class="w-[334px] xl:w-[900px]">
+        <li class="QAcard" @click="isShow = !isShow">
+          <h3 class="xl:text-[1.125rem]" :class="{ active: isShow, none_active: !isShow }">我要怎麼變更方案？</h3>
+          <img
+            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_plus.svg"
+            width="20"         
+            class="w-[16px] xl:w-[20px]"
+          />
+        </li>
+        <li :class="{ QAinfo: isShow, none_QAinfo: !isShow }">
+          <p class="w-full" v-if="isShow">若需要降低人數費用，需支付額外違約額
+          若是提高訂閱人數方案，請直接洽詢客服人約</p>
+        </li>
+        <li class="QAcard">
+          <h3 class="xl:text-[1.125rem]">如果我沒有LINE官方帳號怎麼辦？</h3>
+          <img
+            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_plus.svg"
+            width="20"         
+            class="w-[16px] xl:w-[20px]"
+          />
+        </li>
+        <li class="QAcard">
+          <h3 class="xl:text-[1.125rem]">非上班日有客服支援嗎？</h3>
+          <img
+            src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/f70f00178a7f0baa31e9c01634303d8562cfe93a/chatTalker_images/icon_plus.svg"
+            width="20"         
+            class="w-[16px] xl:w-[20px]"
+          />
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="copyRight">
+    <p>© 2022 chatTalker. All Rights Reserved.</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false
+    }
+
+  }
+}
+</script>
+
+<style>
+.active {
+  transition: 0.2s ease;
+  color: #56C4C5;
+}
+.none_active {
+  transition: 0.2s ease;
+  color: #000
+}
+.QAinfo {
+  transition: all 0.2s ease;
+  width: 100%;
+  height: 50px;
+  overflow: hidden;
+}
+.none_QAinfo {
+  transition: all 0.2s ease;
+  height: 0px;
+  overflow: hidden;
+}
+
+
+
+</style>
+
+
