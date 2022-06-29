083af8fb63f033feafd8780dfd72bc988e95894f
todelusion <todelusion@gmail.com>
2022-06-29 12:24:36 +0800

    header_finished

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,34 @@
 <template>
-<h1>test</h1>
+<div>
+    <header class="banner flex justify-center items-center">
+        <ul class="max-w-[304px] text-white italic">
+            <li class="text-xl">here's</li>
+            <li class="font-black text-7xl mb-8">JOANNE</li>
+            <li class="font-normal text-base mb-10">Founder of “XXXD Official’’＆Graphic Designer</li>
+            <li class="flex justify-evenly ml-[-16px]">
+                <img src="./assets/svg/behance.svg" alt="behance" width="60" class="w-11">
+                <img src="./assets/svg/facebook.svg" alt="behance" width="60" class="w-11">
+                <img src="./assets/svg/instagram.svg" alt="behance" width="60" class="w-11">
+                <img src="./assets/svg/youtube.svg" alt="behance" width="60" class="w-11">
+            </li>
+        </ul>
+    </header>
+    <section>
+        <ul>
+            <li>About</li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </section>
+</div>
 </template>
+
+<style>
+.banner {
+    height: 100vh;
+    background-image: url('./assets/banner.png');
+    background-size: cover;
+    background-position: center;
+}
+</style>
